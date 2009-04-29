2fba9ab9eb1bc6d4430ccd85884249de21fec492
Joshua Lock <josh@linux.intel.com>
2009-04-29 10:32:52 +0100

    Add test for CmService and general test infrastructure
    IMHO the sample/ directory should show useful examples of using the API
    whereas the tests/ directory is a place for me to put hacky programs to
    test API functionality.
    
    Introducing a hacky program to test parts of the CmService objects API.

---
+++ tests/test-service.c
@@ -0,0 +1,97 @@
+#include <glib-object.h>
+#include <glib.h>
+#include <gconnman/gconnman.h>
+
+void
+_pretty_print_service (CmService *service)
+{
+  g_debug ("Service %s found of type %s\nPath is %s\n",
+           cm_service_get_name (service),
+           cm_service_get_type (service),
+           cm_service_get_object_path (service));
+}
+
+void
+_manager_updated_cb (CmManager *manager,
+                     gpointer user_data)
+{
+  g_debug ("Manager updated\n");
+}
+
+void
+_service_updated_cb (CmService *service,
+                     gpointer user_data)
+{
+  g_debug ("Service updated\n");
+  _pretty_print_service (service);
+}
+
+void
+_services_changed_cb (CmManager *manager,
+                      gpointer user_data)
+{
+  GList *services;
+  int i;
+
+  g_debug ("Services changed on manager\n");
+  services = cm_manager_get_services (manager);
+  if (!services)
+  {
+    g_debug ("Service list empty... :-(\n");
+  }
+  else
+  {
+    for (i = 0; i < g_list_length (services); i++)
+    {
+      CmService *service = CM_SERVICE (g_list_nth (services,
+                                                   i)->data);
+      g_signal_connect (G_OBJECT (service),
+                        "service-updated",
+                        G_CALLBACK (_service_updated_cb),
+                        NULL);
+    }
+  }
+}
+
+int
+main (int    argc,
+      char **argv)
+{
+  CmManager *manager;
+  GList *services;
+  GError *error = NULL;
+  GMainLoop *loop;
+
+  g_type_init ();
+
+  manager = cm_manager_new (&error);
+  if (error)
+  {
+    g_debug ("Error initialising manager: %s\n",
+             error->message);
+    g_clear_error (&error);
+
+    return -1;
+  }
+
+  if (manager)
+  {
+    g_debug ("Got Manager interface\n");
+
+    g_signal_connect (G_OBJECT (manager),
+                      "manager-updated",
+                      G_CALLBACK (_manager_updated_cb),
+                      NULL);
+    g_signal_connect (G_OBJECT (manager),
+                      "services-changed",
+                      G_CALLBACK (_services_changed_cb),
+                      NULL);
+    cm_manager_refresh (manager);
+  }
+
+  loop = g_main_loop_new (NULL,
+                          FALSE);
+  g_main_loop_run (loop);
+
+  return 0;
+}
