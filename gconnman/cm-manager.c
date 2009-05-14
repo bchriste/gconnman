ad198fad02ecf950b4b0ad3d927a7bd7413c9f60
Joshua Lock <josh@linux.intel.com>
2009-05-14 22:19:59 +0100

    We are now LGPL 2.1 licensed. Update COPYING and headers

--- gconnman/cm-manager.c
+++ gconnman/cm-manager.c
@@ -1,3 +1,24 @@
+/*
+ * Gconnman - a GObject wrapper for the Connman D-Bus API
+ * Copyright © 2009, Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * Written by:	James Ketrenos <jketreno@linux.intel.com>
+ *		Joshua Lock <josh@linux.intel.com>
+ *
+ */
 
 #include <glib.h>
 #include <string.h> /* strcmp */
